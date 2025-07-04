dfb4bc91103c1c96b2f0aae6c35b28a78785a10f
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-04 16:50:38 +0530

    feat<dev>: Customizing Response Data

--- src/main/java/com/codewithnaveen/ecommerce/dtos/UserDto.java
+++ src/main/java/com/codewithnaveen/ecommerce/dtos/UserDto.java
@@ -1,12 +1,29 @@
 package com.codewithnaveen.ecommerce.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+
+    @JsonIgnore // to exclude the key value from the JSON
     private Long id;
+
+    @JsonProperty("user_name") //to change the key name
     private String name;
+
     private String email;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNumber;
+
+    @JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/codewithnaveen/ecommerce/mappers/UserMapper.java
+++ src/main/java/com/codewithnaveen/ecommerce/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.codewithnaveen.ecommerce.mappers;
 import com.codewithnaveen.ecommerce.dtos.UserDto;
 import com.codewithnaveen.ecommerce.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
