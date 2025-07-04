b1746c7a7238219d9a1df8e428648ccd690f383b
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-04 11:53:12 +0530

    feat<dev>: Creating template and passing data to views

--- src/main/java/com/codewithnaveen/ecommerce/controllers/HomeController.java
+++ src/main/java/com/codewithnaveen/ecommerce/controllers/HomeController.java
@@ -1,19 +1,15 @@
 package com.codewithnaveen.ecommerce.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index(){
-        return "index.html";
+    public String index(Model model){
+        model.addAttribute("name", "Naveen");
+        return "index";
     }
-
-    @RequestMapping("/hello")
-    public String sayHello(){
-        return "index.html";
-    }
-
 }
