124969d10f4d1a4d2adbd1dc23c2487b0ee7cf14
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-04 12:26:48 +0530

    feat<dev>: Creating Dynamic Routes - PathVariable

--- src/main/java/com/codewithnaveen/ecommerce/controllers/UserController.java
+++ src/main/java/com/codewithnaveen/ecommerce/controllers/UserController.java
@@ -4,17 +4,25 @@ import com.codewithnaveen.ecommerce.entities.User;
 import com.codewithnaveen.ecommerce.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getuser(@PathVariable Long id){
+        return userRepository.findById(id).orElse(null);
     }
 
 }
