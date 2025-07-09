a202765c09b8fe04b9531b7d249f3b8a74596f31
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-09 11:42:17 +0530

    feat<dev>: Implementing Custom Validation

--- src/main/java/com/codewithnaveen/ecommerce/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithnaveen/ecommerce/dtos/RegisterUserRequest.java
@@ -1,5 +1,6 @@
 package com.codewithnaveen.ecommerce.dtos;
 
+import com.codewithnaveen.ecommerce.validation.Lowercase;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,6 +13,7 @@ public class RegisterUserRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email must be valid")
+    @Lowercase(message = "Email must be in lowercase")
     private String email;
 
     @NotBlank(message = "Password is required")
