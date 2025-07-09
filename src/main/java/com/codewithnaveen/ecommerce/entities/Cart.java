09a99a091151d17922782a8fdb33fcfcc407371a
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-09 15:59:08 +0530

    feat<dev>: Creating Cart and CartItem entities

---
+++ src/main/java/com/codewithnaveen/ecommerce/entities/Cart.java
@@ -0,0 +1,27 @@
+package com.codewithnaveen.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "date_created", insertable = false, updatable = false)
+    private LocalDate dateCreated;
+
+    private Set<CartItem> cartItems = new LinkedHashSet<>();
+}
