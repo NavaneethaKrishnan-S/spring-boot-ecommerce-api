dabf98390e000adf505d064160fb662a02e8a452
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-14 12:58:40 +0530

    feat<dev>: Implementing - adding a product to the cart

--- src/main/java/com/codewithnaveen/ecommerce/controllers/CartController.java
+++ src/main/java/com/codewithnaveen/ecommerce/controllers/CartController.java
@@ -1,15 +1,20 @@
 package com.codewithnaveen.ecommerce.controllers;
 
+import com.codewithnaveen.ecommerce.dtos.AddItemToCartRequest;
 import com.codewithnaveen.ecommerce.dtos.CartDto;
+import com.codewithnaveen.ecommerce.dtos.CartItemDto;
 import com.codewithnaveen.ecommerce.entities.Cart;
+import com.codewithnaveen.ecommerce.entities.CartItem;
 import com.codewithnaveen.ecommerce.mappers.CartMapper;
 import com.codewithnaveen.ecommerce.repositories.CartRepository;
+import com.codewithnaveen.ecommerce.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/carts")
@@ -17,6 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class CartController {
 
     private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
     private final CartMapper cartMapper;
 
     @PostMapping
@@ -30,5 +36,43 @@ public class CartController {
         var uri = uriBuilder.path("/carts/{id}").buildAndExpand(cartDto.getId()).toUri();
 
         return ResponseEntity.created(uri).body(cartDto);
+    }
+
+    @PostMapping("/{cartId}/items")
+    public ResponseEntity<CartItemDto> addProductToCart(
+            @PathVariable UUID cartId,
+            @RequestBody AddItemToCartRequest request
+            ){
+
+        var cart = cartRepository.findById(cartId).orElse(null);
+        if(cart == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        var product = productRepository.findById(request.getProductId()).orElse(null);
+        if(product == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var cartItem = cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+
+        if(cartItem != null){
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        } else{
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(cart);
+            cart.getCartItems().add(cartItem);
+        }
+
+        cartRepository.save(cart);
+
+        var cartItemDto = cartMapper.toDto(cartItem);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
     }
 }
--- src/main/java/com/codewithnaveen/ecommerce/mappers/CartMapper.java
+++ src/main/java/com/codewithnaveen/ecommerce/mappers/CartMapper.java
@@ -1,11 +1,17 @@
 package com.codewithnaveen.ecommerce.mappers;
 
 import com.codewithnaveen.ecommerce.dtos.CartDto;
+import com.codewithnaveen.ecommerce.dtos.CartItemDto;
 import com.codewithnaveen.ecommerce.entities.Cart;
+import com.codewithnaveen.ecommerce.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
 
     CartDto toDto(Cart cart);
+
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
+    CartItemDto toDto(CartItem cartItem);
 }
