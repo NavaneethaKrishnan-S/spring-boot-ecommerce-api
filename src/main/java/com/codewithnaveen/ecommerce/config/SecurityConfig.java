c0cdf99b7602f06a0da6d26d2c8948d06f7c4e33
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-17 12:42:15 +0530

    feat<dev>: Implementing AuthenticationManager - AuthenticationProvider - DaoAuthenticationProvider

--- src/main/java/com/codewithnaveen/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/codewithnaveen/ecommerce/config/SecurityConfig.java
@@ -1,19 +1,28 @@
 package com.codewithnaveen.ecommerce.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -21,13 +30,26 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationProvider authenticationProvider(){
+        var provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Stateless sessions (token - based authentication)
         http
                 .sessionManagement(c -> c
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
-                .authorizeHttpRequests(c -> c // Authorize
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(c -> c
                         .requestMatchers("/carts/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
@@ -36,5 +58,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
--- src/main/java/com/codewithnaveen/ecommerce/controllers/AuthController.java
+++ src/main/java/com/codewithnaveen/ecommerce/controllers/AuthController.java
@@ -1,39 +1,39 @@
 package com.codewithnaveen.ecommerce.controllers;
 
 import com.codewithnaveen.ecommerce.dtos.UserLoginRequest;
-import com.codewithnaveen.ecommerce.repositories.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<Void> loginRequest(
             @Valid @RequestBody UserLoginRequest request
             ){
 
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if(user == null){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
 
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialExceptions(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
