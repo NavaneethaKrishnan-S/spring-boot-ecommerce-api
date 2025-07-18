14034107507b99d011374853b999a232d5b0bc46
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-07-18 12:07:05 +0530

    feat<dev>: Validating JSON Web tokens

--- src/main/java/com/codewithnaveen/ecommerce/controllers/AuthController.java
+++ src/main/java/com/codewithnaveen/ecommerce/controllers/AuthController.java
@@ -37,6 +37,14 @@ public class AuthController {
         return ResponseEntity.ok(new JwtResponse(token));
     }
 
+    @PostMapping("/validate")
+    public Boolean validate(@RequestHeader("Authorization") String authHeader){
+
+        var token = authHeader.replace("Bearer ", "");
+
+        return jwtService.validateToken(token);
+    }
+
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Void> handleBadCredentialExceptions(){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
