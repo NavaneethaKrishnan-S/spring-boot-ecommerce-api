87041acae4df5f4c10a65df0ebd46b5cb3579bb8
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-08-01 12:32:34 +0530

    feat<dev>: Exercise - Enhancing the JWT Payload

--- src/main/java/com/codewithnaveen/ecommerce/services/JwtService.java
+++ src/main/java/com/codewithnaveen/ecommerce/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.codewithnaveen.ecommerce.services;
 
+import com.codewithnaveen.ecommerce.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
@@ -15,11 +16,15 @@ public class JwtService {
     @Value("${spring.jwt.secret}")
     private String secret;
 
-    final long tokenExpiration = 86400;
 
-    public String generateToken(String email){
+    public String generateToken(User user){
+
+        final long tokenExpiration = 86400;
+
         return Jwts.builder()
-                .subject(email)
+                .subject(user.getId().toString())
+                .claim("email", user.getEmail())
+                .claim("name", user.getName())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
@@ -46,7 +51,7 @@ public class JwtService {
                 .getPayload();
     }
 
-    public String getEmailFromToken(String token){
-        return getClaims(token).getSubject();
+    public Long getUserIdFromToken(String token){
+        return Long.valueOf(getClaims(token).getSubject());
     }
 }
