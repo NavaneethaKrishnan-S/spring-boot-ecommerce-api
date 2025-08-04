00580e9519a0257ac794263930cb91cc13941f93
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-08-04 12:07:12 +0530

    feat<dev>: Refreshing Access Tokens

--- src/main/java/com/codewithnaveen/ecommerce/controllers/AuthController.java
+++ src/main/java/com/codewithnaveen/ecommerce/controllers/AuthController.java
@@ -57,12 +57,19 @@ public class AuthController {
         return ResponseEntity.ok(new JwtResponse(accessToken));
     }
 
-    @PostMapping("/validate")
-    public Boolean validate(@RequestHeader("Authorization") String authHeader){
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtResponse> refresh(
+            @CookieValue(value = "refreshToken") String refreshToken
+    ){
+        if(!jwtService.validateToken(refreshToken)){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
-        System.out.println("Validate Controller called");
-        var token = authHeader.replace("Bearer ", "");
-        return jwtService.validateToken(token);
+        var userId = jwtService.getUserIdFromToken(refreshToken);
+        var user = userRepository.findById(userId).orElseThrow();
+        var accessToken = jwtService.generateAccessToken(user);
+
+        return ResponseEntity.ok(new JwtResponse(accessToken));
     }
     
     @GetMapping("/me")
