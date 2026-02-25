c9f5a7abb0653673196d37a231934c06e244881c
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-25 12:19:05 +0530

    feat<dev>: Creating DTOs for the CheckOut process

---
+++ src/main/java/com/codewithnaveen/ecommerce/dtos/CheckoutRequest.java
@@ -0,0 +1,13 @@
+package com.codewithnaveen.ecommerce.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CheckoutRequest {
+
+    @NotNull(message = "Cart ID is required")
+    private UUID cartId;
+}
