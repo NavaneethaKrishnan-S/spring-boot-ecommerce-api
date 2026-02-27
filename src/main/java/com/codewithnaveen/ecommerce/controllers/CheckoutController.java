b096c4cb58a3f244032f753e90dbd426bf232636
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-27 12:27:31 +0530

    feat<dev>: Creating a CheckOut session

--- src/main/java/com/codewithnaveen/ecommerce/controllers/CheckoutController.java
+++ src/main/java/com/codewithnaveen/ecommerce/controllers/CheckoutController.java
@@ -6,8 +6,12 @@ import com.codewithnaveen.ecommerce.dtos.ErrorDto;
 import com.codewithnaveen.ecommerce.exceptions.CartEmptyException;
 import com.codewithnaveen.ecommerce.exceptions.CartNotFoundException;
 import com.codewithnaveen.ecommerce.services.CheckoutService;
+import com.stripe.exception.StripeException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +22,15 @@ public class CheckoutController {
     private final CheckoutService checkoutService;
 
     @PostMapping
-    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request){
-        return checkoutService.checkout(request);
+    public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request){
+
+        try {
+            return ResponseEntity.ok(checkoutService.checkout(request));
+        } catch (StripeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorDto("Error creating a checkout session"));
+        }
     }
 
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
--- src/main/java/com/codewithnaveen/ecommerce/services/CheckoutService.java
+++ src/main/java/com/codewithnaveen/ecommerce/services/CheckoutService.java
@@ -7,18 +7,26 @@ import com.codewithnaveen.ecommerce.exceptions.CartEmptyException;
 import com.codewithnaveen.ecommerce.exceptions.CartNotFoundException;
 import com.codewithnaveen.ecommerce.repositories.CartRepository;
 import com.codewithnaveen.ecommerce.repositories.OrderRepository;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutService {
     private final CartRepository cartRepository;
     private final OrderRepository orderRepository;
     private final AuthService authService;
     private final CartService cartService;
 
-    public CheckoutResponse checkout(CheckoutRequest request) {
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if(cart == null){
             throw new CartNotFoundException();
@@ -32,8 +40,37 @@ public class CheckoutService {
 
         orderRepository.save(order);
 
-        cartService.clearCart(cart.getId());
+        try{
+            // Create a checkout session
+            var builder = SessionCreateParams.builder()
+                    .setMode(SessionCreateParams.Mode.PAYMENT)
+                    .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                    .setCancelUrl(websiteUrl + "/checkout-cancel");
 
-        return new CheckoutResponse(order.getId());
+            order.getItems().forEach( item -> {
+                var lineItem =  SessionCreateParams.LineItem.builder()
+                        .setQuantity(Long.valueOf(item.getQuantity()))
+                        .setPriceData(
+                                SessionCreateParams.LineItem.PriceData.builder()
+                                        .setCurrency("inr")
+                                        .setUnitAmountDecimal(item.getUnitPrice())
+                                        .setProductData(
+                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                        .setName(item.getProduct().getName())
+                                                        .build()
+                                        ).build()
+                        ).build();
+                builder.addLineItem(lineItem);
+            });
+
+            var session = Session.create(builder.build());
+
+            cartService.clearCart(cart.getId());
+
+            return new CheckoutResponse(order.getId(), session.getUrl());
+        } catch (StripeException ex) {
+            orderRepository.delete(order);
+            throw ex;
+        }
     }
 }
